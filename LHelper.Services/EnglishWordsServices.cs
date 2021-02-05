32e31b863b6f16035636aa9591625fddc98418c0
Marthon <marcibaumel@gmail.com>
2021-02-05 16:13:37 +0100

    services

---
+++ LHelper.Services/EnglishWordsServices.cs
@@ -0,0 +1,28 @@
+﻿using LHelper.DataAccess;
+using LHelper.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LHelper.Services
+{
+    public class EnglishWordsServices
+    {
+        private UnitOfWork unitOfWork;
+
+        public EnglishWordsServices()
+        {
+            unitOfWork = new UnitOfWork();
+        }
+
+        public void AddANewEnglisWord(EnglishWordsModel englishWordsModel)
+        {
+            unitOfWork.englishWordsModels.Add(englishWordsModel);
+            unitOfWork.SaveChanges();
+        }
+
+
+    }
+}
