7c218701573695a77a8e4f6475e82fab5784b7ec
Marthon <marcibaumel@gmail.com>
2021-02-08 00:12:27 +0100

    csv load not working

--- LHelper.WPF/ViewModels/ImportUCViewModel.cs
+++ LHelper.WPF/ViewModels/ImportUCViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,6 @@ namespace LHelper.WPF.ViewModels
 {
     public class ImportUCViewModel: ViewModelBase
     {
+       
     }
 }
--- LHelper.WPF/Views/ImportUC.xaml.cs
+++ LHelper.WPF/Views/ImportUC.xaml.cs
@@ -1,5 +1,10 @@
-﻿using System;
+﻿using CsvHelper;
+using LHelper.WPF.ViewModels;
+using Microsoft.Win32;
+using System;
 using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -20,9 +25,42 @@ namespace LHelper.WPF.Views
     /// </summary>
     public partial class ImportUC : UserControl
     {
+        private ImportUCViewModel importUCViewModel = new ImportUCViewModel();
+        
+
         public ImportUC()
         {
             InitializeComponent();
+        }
+
+        private void CSV_Click(object sender, RoutedEventArgs e)
+        {
+            var filePath = string.Empty;
+            
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.InitialDirectory = "c:\\";
+            openFileDialog.Filter = "CSV Files (*.csv)|*.csv";
+            openFileDialog.RestoreDirectory = true;
+            if (openFileDialog.ShowDialog() == true)
+            {
+                //Get the path of specified file
+                filePath = openFileDialog.FileName;
+                //using (var csv = new CsvReader(filePath, CultureInfo.InvariantCulture))
+                //{
+                //    var records = csv.GetRecords<Foo>();
+                //}
+
+            }
+        }
+
+        private void JSON_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Single_World_Click(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
